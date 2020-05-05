183c4563d35127b6aa7e22f6d95cc1509af8a3ba
Joel Teixeira <joelateixeira7@gmail.com>
2020-05-05 14:37:57 +0100

    Changes in design in the login component

--- projects/joelact/extrack/src/components/user/login/Login.js
+++ projects/joelact/extrack/src/components/user/login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
 import './Login.css';
-import LoginService from '../../../services/user/LoginService'
+import LoginService from '../../../services/user/LoginService';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -9,36 +9,41 @@ export default class Login extends React.Component {
         this.loginService = new LoginService();
     }
 
-    login() {
+    login = () => {
         console.time('perf');
-        this.loginService.login({}).then((data) => {
-            console.log(data);
-            console.timeEnd('perf');
-        }).catch((error) => {
-            console.log(error);
-        })
+        this.loginService
+            .login({})
+            .then((data) => {
+                console.log(data);
+                console.timeEnd('perf');
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
         return (
             <div className="Login">
                 <form>
-                    <FormGroup controlId="username" bsSize="large" className="group">
-                        <FormLabel column="sm" className="label">Username</FormLabel>
-                        <FormControl
-                            autoFocus
-                            type="text"
-                        />
+                    <FormGroup controlId="username" className="group">
+                        <FormLabel column="sm" className="label">
+                            Username
+                        </FormLabel>
+                        <FormControl autoFocus type="text" />
                     </FormGroup>
-                    <FormGroup controlId="password" bsSize="large" className="group">
-                        <FormLabel column="sm" className="label">Password</FormLabel>
-                        <FormControl
-                            type="password"
-                        />
+                    <FormGroup controlId="password" className="group">
+                        <FormLabel column="sm" className="label">
+                            Password
+                        </FormLabel>
+                        <FormControl type="password" />
                     </FormGroup>
-                    <Button block bsSize="large" type="button" onClick={this.login.bind(this)}>
-                        Login
-                    </Button>
+                    <div>
+                        <Button type="button" onClick={this.login}>
+                            Login
+                        </Button>
+                        <Button type="button">Register</Button>
+                    </div>
                 </form>
             </div>
         );
--- projects/joelact/extrack/src/index.js
+++ projects/joelact/extrack/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Login from './components/user/login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
